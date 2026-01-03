3fe00b300f4e7642331b8874a12aafb94fa3887e
ChakornK <chakornk2007@gmail.com>
2026-01-03 13:53:54 -0800

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref, useTemplateRef } from "vue";
+import Renderer from "./components/Renderer.vue";
+
+const equation1 = ref("");
+const equation2 = ref("");
+
+const rendererRef = useTemplateRef("renderer");
+</script>
+
+<template>
+  <main class="flex flex-col items-center gap-2 p-8 text-center">
+    <h1 class="text-4xl">Equation Vis</h1>
+    <h2 class="mb-4">Visualize math equations as a heatmap</h2>
+    <p>Enter equation</p>
+    <p class="text-xs text-neutral-300">
+      Equation should be a valid GLSL expression in terms of x and y
+    </p>
+    <p>
+      <input
+        type="text"
+        class="border border-neutral-700 px-2 py-1 outline-0"
+        v-model="equation1"
+        placeholder="y"
+      />
+      =
+      <input
+        type="text"
+        class="border border-neutral-700 px-2 py-1 outline-0"
+        v-model="equation2"
+        placeholder="pow(x, 2) - 1"
+      />
+    </p>
+
+    <button class="cursor-pointer bg-neutral-800 px-4 py-2" @click="rendererRef?.vis()">
+      Visualize!
+    </button>
+
+    <Renderer :equation1="equation1" :equation2="equation2" ref="renderer" />
+  </main>
+</template>
---
+++ src/components/Renderer.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { onMounted, useTemplateRef } from "vue";
+
+const { equation1, equation2 } = defineProps({
+  equation1: { type: String, required: true },
+  equation2: { type: String, required: true },
+});
+
+const vs = `
+attribute vec2 p;
+void main() {
+  gl_Position = vec4(p, 0, 1);
+}
+`;
+
+const sh = (gl: WebGLRenderingContext, t: GLenum, s: string) => {
+  const x = gl.createShader(t) as WebGLShader;
+  gl.shaderSource(x, s);
+  gl.compileShader(x);
+  return x as WebGLShader;
+};
+
+const canvasRef = useTemplateRef<HTMLCanvasElement>("canvas");
+const vis = () => {
+  const canvas = canvasRef.value;
+  if (!canvas) return;
+  const gl = canvas.getContext("webgl");
+  if (!gl) return alert("WebGL not supported");
+
+  const fs = `
+precision mediump float;
+float scale = 20.0;
+float equation1(float x, float y) {
+  // return x/((x*x)+(y*y));
+  return ${equation1};
+}
+float equation2(float x, float y) {
+  return ${equation2};
+}
+float a(float x, float y) {
+  return abs(equation1((x-256.0)/512.0*scale, (y-256.0)/512.0*scale) - equation2((x-256.0)/512.0*scale, (y-256.0)/512.0*scale) );
+}
+void main() {
+  gl_FragColor = vec4(1.0-a(gl_FragCoord.x, gl_FragCoord.y), 0, 0, 1);
+}
+`;
+
+  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+  gl.clearColor(0, 0, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  const program = gl.createProgram();
+  gl.attachShader(program, sh(gl, gl.VERTEX_SHADER, vs));
+  gl.attachShader(program, sh(gl, gl.FRAGMENT_SHADER, fs));
+  gl.linkProgram(program);
+  gl.useProgram(program);
+
+  const buf = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]), gl.STATIC_DRAW);
+
+  const loc = gl.getAttribLocation(program, "p");
+  gl.enableVertexAttribArray(loc);
+  gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
+
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+  const err = gl.getProgramInfoLog(program);
+  if (err) alert(err);
+};
+onMounted(vis);
+
+defineExpose({ vis });
+</script>
+
+<template>
+  <canvas width="512" height="512" ref="canvas"></canvas>
+</template>
